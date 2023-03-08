fbec2f286f07b37e2b6fd3edaab60ac7f42a55da
derock17 <derekpagel20@gmail.com>
2023-03-08 17:12:32 -0600

    initial commit

---
+++ compile.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const solc = require("solc");
+const fs = require("fs-extra");
+
+const buildPath = path.resolve(__dirname, "ethereum", "build");
+fs.removeSync(buildPath);
+
+const coinCompetitionPath = path.resolve(__dirname, "ethereum", "contracts", "CompetitionCoin.sol");
+const source = fs.readFileSync(coinCompetitionPath, "utf8");
+
+const input = {
+  language: "Solidity",
+  sources: {
+    "CompetitionCoin.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+  "CompetitionCoin.sol"
+];
+
+fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.replace(":", "") + ".json"),
+    output[contract]
+  );
+}
